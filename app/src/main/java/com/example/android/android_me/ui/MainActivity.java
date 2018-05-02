67979180032f1175adffe5b97727f4aa481a9b5f
Roxfives <ricardo.chagas2@outlook.com>
2018-05-02 12:47:49 -0300

    Completes exercise 04

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+public class MasterListFragment extends Fragment {
+    public MasterListFragment() {} // Mandatory empty contructor
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        GridView mGridView = (GridView) rootView.findViewById(R.id.master_fragment_grid);
+
+        MasterListAdapter mMasterListAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        mGridView.setAdapter(mMasterListAdapter);
+
+        return rootView;
+    }
+}
